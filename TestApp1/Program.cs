0d0f26d65fa64f484960b88f9aa8d183db58dea9
Angelina <angelina.zotochkina@yandex.ru>
2024-04-04 09:20:38 +0700

    Добавьте файлы проекта.

---
+++ TestApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp1
+{
+ public class Program
+    {
+       public void Main()
+        {
+            Console.Write("Введите свое имя: ");
+            var name = Console.ReadLine();     
+            Console.WriteLine($"Привет {name}");    
+        }
+    }
+}
